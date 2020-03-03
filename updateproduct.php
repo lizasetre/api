e8d54d9c5bd7f1a7480cd9d2161b034c651e4491
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-03-03 11:00:41 +0800

    Update updateproduct.php

--- updateproduct.php
+++ updateproduct.php
@@ -11,7 +11,7 @@
 
 ?>
 
-<link rel="stylesheet" type="text/css" href="css/styles_products.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
 <form method="POST" action="pro_update.php?id=<?php echo $id ?>">
 	<br>
