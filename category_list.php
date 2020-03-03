6fd1b646ab72ee6fc61dee0d2e345dfd18d8a81a
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-03-03 10:20:17 +0800

    Create category_list.php

---
+++ category_list.php
@@ -0,0 +1,33 @@
+<?php
+	$jsonCategory = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
+	$categoryData = json_decode($jsonCategory,true);
+	$category = $categoryData['records'];
+?>
+
+<link rel="stylesheet" type="text/css" href="css/styles_product.css">
+
+<h1>CATEGORY LIST</h1>
+<BR>
+
+<table>
+		<tr>
+			<td> <b>ID</b> </td>
+			<td> <b>Name</b> </td>
+			<td> <b>Description</b> </td>
+		</tr>
+<?php 
+	foreach($category as $result)
+	{
+?>
+		<tr>
+      		<td><?php echo $result['id']; ?> </td>
+     		<td><?php echo $result['name']; ?> </td>
+      		<td><?php echo $result['description'];?> </td>
+		</tr>
+<?php
+	}
+?>
+</table>
+<br>
+<br>
+<br>
