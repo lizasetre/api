7d1b5a7d287880c9a54060e5ee3634ad833b0476
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-03-03 10:20:49 +0800

    Create deleteproduct.php

---
+++ deleteproduct.php
@@ -0,0 +1,26 @@
+<?php
+	$id = $_GET['id'];
+ 	$json = file_get_contents('http://rdapi.herokuapp.com/product/read_one.php?id='.$id);
+ 	$data = json_decode($json,true);
+ 	$details = array('records' => $data);
+ 	$result = $details['records'];
+?>
+
+<link rel="stylesheet" type="text/css" href="css/styles_product.css">
+
+<html>
+	<head> 
+		<title> Delete Confirm </title>
+		<link rel="stylesheet" type="text/css" href="css/styles_supplier.css">
+	</head>
+	<body>
+		<h1>Are you sure to delete <?php echo $result['name']?>?</h1>
+		<br>
+		<form action = "pro_delete.php?id=<?php echo $id?>" method = "POST">
+			<input class="delete" type="Submit" name="Submit" value="Yes">
+		</form>
+		<form action="index.php?page=Product">
+			<button class="delete" type="submit"> No </button>
+		</form>
+	</body>
+</html>
