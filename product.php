4142ae16653365e4edc65fcb758d68efef1c225f
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-03-05 10:24:53 +0800

    Update product.php

--- product.php
+++ product.php
@@ -16,26 +16,38 @@ if(isset($_POST['search'])){
 }
 ?>
 
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+
 <h1> Product List </h1>
 
-<form action="index.php?navigation=product" method="POST">
-	Search:<input type="text" name="search" placeholder="Enter Product Name">
-		<input type="submit" name="submit" value="Search">
-	</form>
 
-<table>
+    <div id="cover">
+    <form action="index.php?page=product" method="POST">
+   Search:<input type="text" name="search" placeholder="Enter Product Name">
+		<input type="submit" name="submit" value="Search">
+  </form>
+</div>
+
+<table id="list">
     <tr>
         <th>ID</th>
         <th>Product</th>
+        <th>Description</th>
         <th>Price</th>
+        <th>Category</th>
     </tr>
 <?php
 foreach($list as $value){
     ?>
     <tr>
         <td><?php echo $value['id'];?></td>
-        <td><a href="product-details.php?id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
+        <td id ="link"><a href="product-details.php?id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
+        <td><?php echo $value['description'];?></td>
         <td><?php echo $value['price'];?></td>
+        <td><?php echo $value['category_name'];?></td>
     </tr>
 <?php
 }
