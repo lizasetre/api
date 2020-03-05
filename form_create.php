6dd8fced4d352da551f82464c5cd96f993129d8a
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-03-05 10:26:27 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -4,24 +4,24 @@
 	$data = json_decode($json,true);
 	$category = $data['records'];
 ?>
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-<form action="pro_create.php" method="POST">
-<h1> Create Product </h1>
-	  <div style="margin: auto; width:15%; border:3px solid black;padding: 10px;">
-<input type="text" name="name" placeholder="name"/><br><br>
-<input type="text" name="description" placeholder="description"/><br><br>
-<input type="text" name="price" placeholder="price"/><br><br>
-<select name="category"><br><br>
-		</div>
-<option value="">--Category--</option>
-	<?php
-      foreach($category as $cview){
-    ?>
-		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
-    <?php
-      }
-    ?>
-	</select>
-<input type="submit" name="submit" value="submit"/>
+<br>
+<div class="form-style-5">
+			<form action="pro_create.php" method="POST">
+			<h1> Add Product </h1>
+			Product:<input type="text" name="name" placeholder="name"/>
+			Description:<input type="text" name="description" placeholder="description"/>
+			Price:<input type="text" name="price" placeholder="price"/>
+			Category:<select name="category">
+			<option value="">--Category--</option>
+				<?php
+				foreach($category as $cview){
+				?>
+					<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
+				<?php
+				}
+				?>
+				</select>
+			<input type="submit" name="submit" value="submit"/>
 
-</form>
+			</form>
+</div>			
